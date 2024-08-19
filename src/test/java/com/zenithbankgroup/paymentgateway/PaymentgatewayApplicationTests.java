93cd9472d17b0951db5fb07a310efb6f09268cdf
sfxcegbujio <cegbujio@seamfix.com>
2024-08-19 15:26:30 +0100

    first commit

---
+++ src/test/java/com/zenithbankgroup/paymentgateway/PaymentgatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zenithbankgroup.paymentgateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaymentgatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

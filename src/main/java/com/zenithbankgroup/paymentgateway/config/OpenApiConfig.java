b221a801ce87f69c2b33f851d221efcb88629b7f
sfxcegbujio <cegbujio@seamfix.com>
2024-08-19 17:55:55 +0100

    added open api config for api documentation

---
+++ src/main/java/com/zenithbankgroup/paymentgateway/config/OpenApiConfig.java
@@ -0,0 +1,7 @@
+package com.zenithbankgroup.paymentgateway.config;/**
+ * @author Chukwuka Justine Egbujio.
+ * @created 19/08/2024 5:52 PM
+ * @project paymentgateway
+ */
+public class OpenApiConfig {
+}
